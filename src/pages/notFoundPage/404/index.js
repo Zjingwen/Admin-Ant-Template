d8aeaa147d5f5c14cd4924db381726f8e81ff203
静文（jingwen） <jingwen@xiaoshijie.com>
2019-08-14 18:31:46 +0800

    release: 调整404页面

--- src/pages/notFoundPage/404/index.js
+++ src/pages/notFoundPage/404/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { connect } from 'dva';
 import HOCpage from '@hoc/HOCpage';
 @HOCpage
-class IndexPage extends React.Component {
+export default class extends React.Component {
   render(){
     return (
       <>你来到了一个神奇的404</>
@@ -10,4 +9,3 @@ class IndexPage extends React.Component {
   };
 }
 
-export default connect()(IndexPage);
